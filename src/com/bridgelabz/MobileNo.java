d9ffdaafccfa3b08b9b0a6b364903e3dec708293
Pooja Katkar <104882596+KatkarSaura@users.noreply.github.com>
2022-09-07 16:55:43 +0530

    Lamda expression

---
+++ src/com/bridgelabz/MobileNo.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+@FunctionalInterface
+interface IMobileNumber {
+    public String validate(String regex, String test);
+}
+
+public class MobileNo {
+    public static void main(String[] args) {
+
+        System.out.println("enter mobile number");
+        Scanner scanner = new Scanner(System.in);
+        String mobileNumber = scanner.next();
+        IFirstName isValid = (pattern,name) ->{
+            return "Given mobile number is " + Pattern.compile(pattern).matcher(name).matches();
+        };
+        System.out.println(isValid.validate("^[0-9]{2}\\s{1}[6-9]{1}[0-9]{9}$", mobileNumber));
+    }
+
+}
